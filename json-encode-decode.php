7d7b11d511e24df2e138aabc31c9ab34d5644e8c
Engr Jp Munoz <mjonellepeter@gmail.com>
2022-06-06 07:04:36 +0800

    Update json-encode-decode.php

--- json-encode-decode.php
+++ json-encode-decode.php
@@ -1,7 +1,11 @@
 <?php
 
-<?php
-$mayor = array("Eric Adams"=>0, "Bill de Blasio"=>8, "Michael Bloomberg"=>12, "Rudy Giuliani"=>8);
-
+#This example shows how to encode an associative array into a JSON object:
+$mayor = array("Eric Adams"=>0, "Bill de Blasio"=>8, "Michael Bloomberg"=>12, "Rudy Giuliani"=>8, "David Dinkins"=>4, "Ed Koch"=>;
 echo json_encode($mayor);
+
+               
+#This example shows how to encode an indexed array into a JSON array:
+$newyorkmayor = array("Eric Adams", "Bill de Blasio", "Michael Bloomberg", "Rudy Giuliani", "David Dinkins", "Ed Koch");
+echo json_encode($newyorkmayor);
 ?>
